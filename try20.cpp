5c40d86c73d64202aba59c912bdd3d65160b89a7
srinivasambore <srinivasambore8@gmail.com>
2025-05-27 11:22:00 +0530

    day5

---
+++ try20.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string x = "10";
+    string y = "20";
+    string z = x + y;
+    cout << z;
+
+    int a = 10;
+    int b = 20;
+    int c = a + b;
+    cout << c;
+    return 0;
+}
---
+++ try25.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string txt = "The character \\ is called backslash.";
+    cout << txt;
+    return 0;
+}
